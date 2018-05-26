f9c8fde46113774b7edcf1bb79e0a710f18bd54c
jinadi yasiruka <jinyyash@gmail.com>
2018-05-26 23:05:20 +0530

    v

---
+++ my-app/src/component/Burgers/BuildControls/BuildControls.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Jinadi on 5/26/2018.
+ */
+import React from 'react';
+const buildControls=(props)=>{
+    <div></div>
+};
+
+export default buildControls;
--- my-app/src/component/Burgers/Burger.js
+++ my-app/src/component/Burgers/Burger.js
@@ -6,11 +6,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 const burger=(props)=>{
+    let transformedIngredients=Object.keys(props.ingredients)
+        .map(igkey=>{
+            return [...Array(props.ingredients[igkey])].map((_,i)=>{
+               return  <BurgerIngredient key={igkey+i} type={igkey}/>
+            });
+        }).reduce((ar,el)=>{
+            return ar.concat(el);
+        },[]);
+    if(transformedIngredients.length==0){
+        transformedIngredients=<p>Please Add Ingredients</p>
+    }
     return(
         <div className={classes.Burger} >
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
